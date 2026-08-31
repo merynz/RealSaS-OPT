08c8f7ff0f77ee0f9da28cb34d338c79b824e229
RealSaS - OPT <140457344+merynz@users.noreply.github.com>
2026-09-01 00:06:21 +0300

    Expose MWB0 compiler facade functions

--- compiler/realsas_compiler_core/api.py
+++ compiler/realsas_compiler_core/api.py
@@ -2,7 +2,17 @@ from __future__ import annotations
 from .surface import build_surface_from_persistence, rigging_surface_from_d2_arrays
 from .rig import qualify_skeleton
 from .skin import qualify_skin
-from .product import assemble_product, bind_proof, require_current_proof, project_runtime_package
+from .mesh_binding import (
+    mesh_candidate_lineage_hash,
+    mesh_lineage_hash,
+    mesh_skin_lineage_hash,
+    validate_surface_support_binding,
+    derive_bound_position,
+    validate_mesh_candidate,
+    validate_qualified_mesh,
+    validate_qualified_mesh_skin,
+)
+from .product import assemble_product, assemble_product_v2, bind_proof, require_current_proof, project_runtime_package
 from .bundle_routes import write_typed_artifact, route_for
 
 class CompilerFacade:
@@ -15,7 +25,16 @@ class CompilerFacade:
     rigging_surface_from_d2_arrays=staticmethod(rigging_surface_from_d2_arrays)
     qualify_skeleton=staticmethod(qualify_skeleton)
     qualify_skin=staticmethod(qualify_skin)
+    mesh_candidate_lineage_hash=staticmethod(mesh_candidate_lineage_hash)
+    mesh_lineage_hash=staticmethod(mesh_lineage_hash)
+    mesh_skin_lineage_hash=staticmethod(mesh_skin_lineage_hash)
+    validate_surface_support_binding=staticmethod(validate_surface_support_binding)
+    derive_bound_position=staticmethod(derive_bound_position)
+    validate_mesh_candidate=staticmethod(validate_mesh_candidate)
+    validate_qualified_mesh=staticmethod(validate_qualified_mesh)
+    validate_qualified_mesh_skin=staticmethod(validate_qualified_mesh_skin)
     assemble_product=staticmethod(assemble_product)
+    assemble_product_v2=staticmethod(assemble_product_v2)
     bind_proof=staticmethod(bind_proof)
     require_current_proof=staticmethod(require_current_proof)
     project_runtime_package=staticmethod(project_runtime_package)

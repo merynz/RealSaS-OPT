e840d99ff3210e1bfd1b42ed2d702c3b15f0a5e4
RealSaS - OPT <140457344+merynz@users.noreply.github.com>
2026-09-05 01:51:24 +0300

    Expose zero-surface compaction and normal operator

--- models/iris/v3/__init__.py
+++ models/iris/v3/__init__.py
@@ -4,14 +4,20 @@ from .scene_first_signed_v3 import (
 )
 from .zero_surface_decoder_v3 import (
     ZeroSurfaceMeshV3,
+    CompactZeroSurfaceV3,
     dense_signed_grid_v3,
     extract_zero_surface_mesh_v3,
+    robust_zero_surface_normals_v3,
+    compact_zero_surface_mesh_v3,
 )
 
 __all__ = [
     "SceneFirstSignedGeometryConfigV3",
     "SceneFirstSignedGeometryV3",
     "ZeroSurfaceMeshV3",
+    "CompactZeroSurfaceV3",
     "dense_signed_grid_v3",
     "extract_zero_surface_mesh_v3",
+    "robust_zero_surface_normals_v3",
+    "compact_zero_surface_mesh_v3",
 ]

f83d95405f7163ab214808de1b053dcc6c00c2a9
RealSaS - OPT <140457344+merynz@users.noreply.github.com>
2026-09-05 01:48:13 +0300

    Expose promoted IRIS v3 geometry package

---
+++ models/iris/v3/__init__.py
@@ -0,0 +1,17 @@
+from .scene_first_signed_v3 import (
+    SceneFirstSignedGeometryConfigV3,
+    SceneFirstSignedGeometryV3,
+)
+from .zero_surface_decoder_v3 import (
+    ZeroSurfaceMeshV3,
+    dense_signed_grid_v3,
+    extract_zero_surface_mesh_v3,
+)
+
+__all__ = [
+    "SceneFirstSignedGeometryConfigV3",
+    "SceneFirstSignedGeometryV3",
+    "ZeroSurfaceMeshV3",
+    "dense_signed_grid_v3",
+    "extract_zero_surface_mesh_v3",
+]

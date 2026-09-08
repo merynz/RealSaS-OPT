94e3d9e2d43b73bb3886c5550c0dd8504bfb3294
RealSaS - OPT <140457344+merynz@users.noreply.github.com>
2026-09-09 01:35:45 +0300

    export SkinTokens-strength codec v3

---
+++ models/skin_field_codec/v3/__init__.py
@@ -0,0 +1,4 @@
+from .config_v3 import SkinTokensStrengthConfigV3
+from .skin_field_codec_v3 import SkinFieldCodecV3, FiniteScalarQuantizerV3, scalar_field_loss_v3
+
+__all__ = ["SkinTokensStrengthConfigV3", "SkinFieldCodecV3", "FiniteScalarQuantizerV3", "scalar_field_loss_v3"]

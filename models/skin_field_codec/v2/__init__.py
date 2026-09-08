3f73de50b9b0c89511459fb51c750bcdcd2461d8
RealSaS - OPT <140457344+merynz@users.noreply.github.com>
2026-09-09 00:42:13 +0300

    feat: export SkinFieldCodec v2

---
+++ models/skin_field_codec/v2/__init__.py
@@ -0,0 +1,19 @@
+from .config_v2 import (
+    EXPECTED_SKIN_FIELD_CODEC_V2_CONFIG_HASH,
+    SKIN_FIELD_CODEC_V2,
+    SkinFieldCodecConfigV2,
+)
+from .skin_field_codec_v2 import (
+    SkinFieldCodecOutputV2,
+    SkinFieldCodecV2,
+    skin_field_codec_loss_v2,
+)
+
+__all__ = [
+    "EXPECTED_SKIN_FIELD_CODEC_V2_CONFIG_HASH",
+    "SKIN_FIELD_CODEC_V2",
+    "SkinFieldCodecConfigV2",
+    "SkinFieldCodecOutputV2",
+    "SkinFieldCodecV2",
+    "skin_field_codec_loss_v2",
+]

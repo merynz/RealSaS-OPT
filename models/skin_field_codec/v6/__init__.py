e8f229226caadcc3b63d42ebb3687889515c99f4
RealSaS - OPT <140457344+merynz@users.noreply.github.com>
2026-09-09 04:19:51 +0300

    export forced-field Arachne skin field v6

---
+++ models/skin_field_codec/v6/__init__.py
@@ -0,0 +1,15 @@
+from .skin_field_codec_v6 import (
+    ArachneSkinFieldConfigV6,
+    ForcedFieldCrossReadoutV6,
+    ScalarFieldQueryDecoderV6,
+    SkinFieldCodecV6,
+    scalar_field_loss_v6,
+)
+
+__all__ = [
+    "ArachneSkinFieldConfigV6",
+    "ForcedFieldCrossReadoutV6",
+    "ScalarFieldQueryDecoderV6",
+    "SkinFieldCodecV6",
+    "scalar_field_loss_v6",
+]

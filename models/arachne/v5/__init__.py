5b5a13d8e5569985fde73a25189683f96749c658
RealSaS - OPT <140457344+merynz@users.noreply.github.com>
2026-09-11 22:24:58 +0300

    Expose Arachne V5 checkpoint authority

--- models/arachne/v5/__init__.py
+++ models/arachne/v5/__init__.py
@@ -6,10 +6,12 @@ from .arachne_candidate_v5 import (
     ArachneA1V5,
     DirectSimplexDecoderV5,
 )
+from .checkpoint_authority_v1 import sealed_arachne_v5_fit1_authority
 
 __all__ = [
     "ArachneA1ConfigV5",
     "ArachneA1OutputV5",
     "ArachneA1V5",
     "DirectSimplexDecoderV5",
+    "sealed_arachne_v5_fit1_authority",
 ]

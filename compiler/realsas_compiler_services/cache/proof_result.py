25d1b5022099c623e2f6b3dafaf79d7699e2bb34
RealSaS - OPT <140457344+merynz@users.noreply.github.com>
2026-09-18 23:04:48 +0300

    cache: drop absent historical render fingerprint

--- compiler/realsas_compiler_services/cache/proof_result.py
+++ compiler/realsas_compiler_services/cache/proof_result.py
@@ -41,7 +41,6 @@ _PROOF_SOURCE_MODULES = (
     "compiler.realsas_compiler_core.v4",
     "compiler.realsas_compiler_core.deformation",
     "compiler.realsas_compiler_core.product_authority_v1",
-    "compiler.realsas_compiler_core.product_external_render",
     "compiler.realsas_compiler_services.proof.failure_signatures",
     "compiler.realsas_compiler_services.proof.motion_bake",
     "compiler.realsas_compiler_services.proof.motion_frame_metrics",
